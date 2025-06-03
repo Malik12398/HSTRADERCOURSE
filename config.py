d7167853e35d7ea926a4e3a2b4859deea13d212a
Malik12398 <technshield007@gmail.com>
2025-06-03 23:13:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 API_ID = 29676661  # my.telegram.org se liya hua
 API_HASH = "1b8fb46782cc52ced8510087c12859b3"  # my.telegram.org se liya hua
 BOT_TOKEN = "7802794930:AAGNO7IK_DB5chmCruiXOmfIPdSW_Phet4g"  # @BotFather se liya hua
-
+GROUP_ID = -1002655615295
 # Pocket Option Bot Username
 PO_BOT_USERNAME = "@AffiliatePocketBot"  # Official bot ka username
